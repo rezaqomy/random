0febce4606cf84ebc4e0632cb754ad57767b1e86
CodeWriter21 <CodeWriter21@gmail.com>
2024-05-19 15:40:25 +0330

    Added the basics for the Telegram bot.

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+
+import log21
+from dotenv import load_dotenv
+from telegram import Update, ForceReply
+from telegram.ext import (Application, ContextTypes, CommandHandler, MessageHandler,
+                          filters)
+
+logger = log21.get_logger()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    logger.info(f"{user.username} started the bot!")
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!", reply_markup=ForceReply(selective=True)
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    load_dotenv()
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(os.getenv("TOKEN")).build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
